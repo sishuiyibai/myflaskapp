e417ac4e8ac246e58158e8e1bdc3107a98e87e4b
sishuiyibai <sishuiyibai@163.com>
2018-03-31 14:04:51 +0800

    将myflaskapp.py重命名为hello.py,将hello_world()函数名修改为hello(),开启DEBUG模式

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!</h1>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
