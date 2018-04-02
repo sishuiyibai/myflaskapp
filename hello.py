8249e3f42ed492a3f605db4fef5ef9d3a1c74b36
Liu <sishuiyibai@163.com>
2018-04-02 17:23:44 +0800

    添加404,500响应代码

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +8,16 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'),500
 
 
 @app.route('/user/<name>')
