48d0d9f27c848fb7ef3add7956f8d2c77fddaca9
Liu <sishuiyibai@163.com>
2018-04-03 16:09:46 +0800

    优化代码，对index.html视图函数添加基于用户会话session的页面重定向功能。

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
-from flask_wtf import Form
+#  from datetime import datetime
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 
 
 # 定义表单类
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
@@ -24,12 +24,11 @@ class NameForm(Form):
 # index.html视图处理函数
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 # 404响应处理函数
