0124b0a7e76cc36414642adb56c1f863127f084f
Liu <sishuiyibai@163.com>
2018-04-04 17:16:44 +0800

    优化代码

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_script import Manager,Shell
+from flask_script import Manager, Shell
 
 
 app = Flask(__name__)
