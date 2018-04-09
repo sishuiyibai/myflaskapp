90998c0c1ed5fb66c4e1ef52e8e6bd144c9aa22a
Liu <sishuiyibai@163.com>
2018-04-09 09:27:32 +0800

    # 注册auth蓝本

--- app/__init__.py
+++ app/__init__.py
@@ -25,9 +25,13 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
-    # 注册蓝本
+    # 注册main蓝本
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # 注册auth蓝本
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     #  附加路由和自定义的错误页面
 
