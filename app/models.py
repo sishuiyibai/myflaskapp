52fa7e9607a3b86e0eba77580ece38e9159b0460
Liu <sishuiyibai@163.com>
2018-04-09 16:04:46 +0800

    1)数据库模型添加 UserMixin 类，以便支持用户登录认证 2)修改users表，添加email字段 3)添加加载用户对象的回调函数load_user（）

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
 #  定义数据库模型
@@ -14,9 +16,10 @@ class Role(db.Model):
         return '<Role %r>' % self.__name__
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
@@ -34,3 +37,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+
