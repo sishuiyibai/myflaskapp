5d2771155cb635ff96ced6a31c6092de3e2357fe
Liu <sishuiyibai@163.com>
2018-04-11 09:19:21 +0800

    1）优化代码，修改用户注册视图函数，添加用户id邮件验证功能； 2）添加用户邮件验证链接处理视图函数，对点击链接后的用户进行验证处理； 3）添加未确认的账户页面跳转处理视图函数， 4）添加未确认用户账户页面视图函数；

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .. import db
 from .forms import LoginForm, RegistrationForm
+from ..email import send_email
 
 
 # 登陆用户
@@ -14,10 +15,12 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+            # 获取客户端下一个请求
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
             return redirect(next)
+            # return redirect('main.index')
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
@@ -37,7 +40,58 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        # 将通过表单验证的用户添加入数据库
         db.session.add(user)
-        flash('You can now login.')
+        db.session.commit()
+        # 生成用户邮件验证id加密令牌字符串
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+# 用户点击邮件链接，请求确认用户的账户
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        # 通过验证
+        flash('You have confirmed your account.Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+# 处理未确认用户账户
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+# 重新发送新的账户确认邮件
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    # 当重新发送账户确认邮件之后，由于当前用户账户仍然没有确认，所以页面跳转被before_app_request（）限制在auth/unconfirmed.html下，
+    # 不会跳转至main/index.html,只有用户打开邮件链接，验证通过后，才会跳转至主页面
+    return redirect(url_for('main.index'))
+
+
+
