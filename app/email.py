28319db1c1fd013f9ebc28a9b8e4a77e27214a33
Liu <sishuiyibai@163.com>
2018-04-11 09:08:35 +0800

    优化代码，无功能性改变

--- app/email.py
+++ app/email.py
@@ -16,6 +16,7 @@ def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
+    # 创建线程
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
