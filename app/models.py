f72c987bef7bf9a3a4dc1080af8b3f222bd0ebe4
Liu <sishuiyibai@163.com>
2018-04-11 16:49:52 +0800

    1）添加用户密码充重置身份确认令牌密钥生成函数； 2）添加用户密码重置身份确认令牌解密及密码修改会话处理函数；

--- app/models.py
+++ app/models.py
@@ -38,18 +38,18 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    # 用户邮件确认生成函数
+    # 用户邮件确认令牌密钥生成函数
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         # 对用户id进行加密签名，随后进行序列化生成令牌字符串
-        return s.dumps({'confirm': self.id})
+        return s.dumps({'confirm': self.id}).decode('utf-8')
 
-    # 用户邮件确认处理函数
+    # 用户邮件确认令牌密钥处理函数
     def confirm(self, token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             # 反序列化
-            data = s.loads(token)
+            data = s.loads(token.encode('utf-8'))
         except:
             return False
         if data.get('confirm') != self.id:
@@ -57,6 +57,27 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         db.session.commit()
+        return True
+
+    # 用户密码充重置身份确认令牌密钥生成函数
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')
+
+    # 用户密码重置身份确认令牌解密及密码修改会话处理函数
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        # 根据令牌反序列化后的用户id值获取当前用户对象
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
         return True
 
     def __repr__(self):
