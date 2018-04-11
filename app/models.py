dc4cb8d830bacf7c31fa037ece12c1dd9e5d29fd
Liu <sishuiyibai@163.com>
2018-04-11 09:13:15 +0800

    修该用户模型，users表添加账户确认comfirmed字段，并添加账户确认加密生成及处理函数。

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 #  定义数据库模型
@@ -23,6 +25,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -34,6 +37,26 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # 用户邮件确认生成函数
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        # 对用户id进行加密签名，随后进行序列化生成令牌字符串
+        return s.dumps({'confirm': self.id})
+
+    # 用户邮件确认处理函数
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            # 反序列化
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
