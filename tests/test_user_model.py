6949bd2dc8c0bda70e26d86ef7fe6b64d2677a83
Liu <sishuiyibai@163.com>
2018-04-11 09:20:35 +0800

    添加用户账户确认功能单元测试函数

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+from app import create_app, db
 from app.models import User
 
 
@@ -21,3 +23,32 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password = 'cat')
         u2 = User(password = 'cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
+        u2 = User(password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertTrue(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        # 设置令牌过期时间为1秒
+        token = u.generate_confirmation_token(1)
+        # 等待2秒
+        time.sleep(2)
+        # 令牌验证
+        self.assertTrue(u.confirm(token))
+
+
