16d7ff247bd568bb56175e7ef5031146e7ff0507
Liu <sishuiyibai@163.com>
2018-04-13 11:36:26 +0800

    将权限类添加入全局变量中，在所有模版中全局可访问

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,15 @@
 from flask import Blueprint
+from ..models import Permission
 
 
 #  创建蓝本
 main = Blueprint('main', __name__)
+
+
+# 使用上下文处理器，将Permission类在所有模版中全局可访问
+@main.app_context_processor
+def inject_permission():
+    return dict(Permission=Permission)
+
 
 from . import views, errors
