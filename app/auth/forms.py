3d1b87dea67d6ee76221835b8c54dbc9a6e3d960
Liu <sishuiyibai@163.com>
2018-04-13 15:34:05 +0800

    优化代码排版

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,10 +16,12 @@ class LoginForm(FlaskForm):
 # 用户注册表单类
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    username = StringField('Username',validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                                                                       'Usernames must have only letters,'
-                                                                                       'numbers,dots or underscores')])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
+                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$',
+                                                          0, 'Username must have only letters,'
+                                                             'numbers,dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
