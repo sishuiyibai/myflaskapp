ef9d7ba60fa278b864647e0614f4ed7b709a11d4
Liu <sishuiyibai@163.com>
2018-04-16 09:56:36 +0800

    添加管理员资料编辑视图路由函数

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, abort, flash, redirect, url_for
 from app import db
 from . import main
-from .forms import EditProfileForm
-from ..models import User
+from .forms import EditProfileForm, EditProfileAdminForm
+from ..models import User, Role
 from flask_login import login_required, current_user
+from ..decorators import admin_required
 
 
 # index.html视图处理函数
@@ -40,5 +41,38 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+# 管理员用户编辑资料路由
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        # 会话内容保存到数据库中
+        db.session.commit()
+        flash('The profile has been updated.')
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
+
+
+
+
 
 
