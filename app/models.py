ceda9f33f11f1fda50b3a8a0828a7cf6c755ddc1
Liu <sishuiyibai@163.com>
2018-04-16 16:06:13 +0800

    创建新分支，分支名为：10d,users表添加邮件hash值链接字段

--- app/models.py
+++ app/models.py
@@ -77,6 +77,8 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    # 用户邮箱号hash值存储字段
+    avatar_hash = db.Column(db.String(32))
 
     # 调用基类的构造函数，定义默认的用户角色
     def __init__(self, **kwargs):
@@ -86,6 +88,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     # 验证用户是否具有指定权限
     def can(self, permissions):
@@ -168,6 +172,7 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
@@ -182,7 +187,7 @@ class User(UserMixin, db.Model):
             url = 'http://secure.gravatar.com/avatar'
         else:
             url = 'http://www.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
