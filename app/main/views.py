a1fcef23e2c5349c2befd5e60fb8c3deb1deb2d0
Liu <sishuiyibai@163.com>
2018-04-20 16:57:55 +0800

    博客文章路由及视图处理函数添加评论表单处理及分页显示功能代码

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, abort, flash, redirect, url_for, request, current_app, make_response
 from app import db
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm, PostForm
-from ..models import User, Role, Post, Permission
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
+from ..models import User, Role, Post, Permission, Comment
 from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
 
@@ -102,8 +102,26 @@ def edit_profile_admin(id):
 @main.route('/post/<int:id>', methods=['GET', 'POST'])
 def post(id):
     post = Post.query.get_or_404(id)
+    # 创建评论表单实例对象
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author = current_user._get_current_object())
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been published.')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1)/current_app.config['FLASK_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['FLASK_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
     # 传入视图渲染页面时posts必须以列表的形式传入，因为内部_post.html需要传入列表形式的posts
-    return render_template('post.html', posts=[post])
+    return render_template('post.html', posts=[post], form=form,
+                           comments=comments, pagination=pagination)
 
 
 # 博客文章的链接视图处理函数
