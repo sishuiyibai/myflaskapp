117f2599aec7bee75b9f89f6bbcb892e440f1f90
Liu <sishuiyibai@163.com>
2018-04-27 16:14:13 +0800

    修改排版

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -6,22 +6,22 @@ from app.models import User, Role, Permission, AnonymousUser
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         with self.assertRaises(AttributeError):
             u.password
 
     def test_password_verification(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
-        u = User(password = 'cat')
-        u2 = User(password = 'cat')
+        u = User(password='cat')
+        u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
 
     def test_valid_confirmation_token(self):
