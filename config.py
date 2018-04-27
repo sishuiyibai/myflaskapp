365416d269e4bf795f67c74de1504ba3906a5daf
Liu <sishuiyibai@163.com>
2018-04-27 16:13:15 +0800

    排版

--- config.py
+++ config.py
@@ -23,13 +23,13 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'None')
     # 邮件账户的密码
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'None')
-    #  flask-mail主题前缀/发邮件人等配置
+    # flask-mail主题前缀/发邮件人等配置
     FLASK_MAIL_SUBJECT_PREFIX = '[Flask]'
     FLASK_MAIL_SENDER = 'sishuiyibai@163.com'
     FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
     #  数据库配置
     # flask-sqlalchemy 2.0 版本以上被移除
-    #SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 分页显示设置
     FLASK_POSTS_PER_PAGE = 5
@@ -43,7 +43,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    #  数据库配置
+    # 数据库配置
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
